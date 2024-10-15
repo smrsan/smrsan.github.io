3c76e976f14253a4bf4d016e717f75620fdc9607
SMRSAN <57676740+smrsan@users.noreply.github.com>
2024-10-15 14:53:04 +0330

    Add some initial reusable structure to the Experience comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 
+import stormLogo from "/logos/storm-online-logo.svg?url";
+
 function App() {
     return (
         <>
             <Hero />
-            <Experience />
+            <Experience
+                name="storm-online"
+                logoUrl={stormLogo}
+                corpTitle={"Rayaneshe Abri-E Yekta Shive "}
+                corpLinkTitle={"storm-online.ir"}
+                corpLinkUrl={
+                    "https://accounting.storm-online.ir/auth/#!/Authentication/Login"
+                }
+                jobTitle={"Full Stack Web Developer "}
+                skillLevel={"Intern/Junior"}
+                description={
+                    <>
+                        Served as a Full-stack JavaScript Developer, reporting
+                        directly to the CEO.
+                        <ul>
+                            <li>
+                                Developed SPA for Accounting User Panel using
+                                Angular.js v1.
+                            </li>
+                            <li>
+                                Developed the server-side RESTful API features
+                                using Express.js.
+                            </li>
+                            <li>
+                                DeImplemented server-side rendering of front-end
+                                HTML templates with EJS.
+                            </li>
+                            <li>
+                                Developed landing page independently using HTML,
+                                Sass, JS, and jQuery.
+                            </li>
+                        </ul>
+                    </>
+                }
+            />
         </>
     );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,42 @@
-const Experience = () => {
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Experience = ({
+    name,
+    logoUrl,
+    corpTitle,
+    corpLinkTitle,
+    corpLinkUrl,
+    jobTitle,
+    skillLevel,
+    description,
+}) => {
+    useGSAP(() => {
+        gsap.to(`#${name}-experience-container`, {
+            translateX: 0,
+            opacity: 1,
+            scrollTrigger: {
+                trigger: `#${name}-experience-spacer`,
+                start: "-5% center",
+                end: "5% center",
+                scrub: true,
+            },
+        });
+
+        gsap.to(`#${name}-experience-logo`, {
+            opacity: 1,
+            scrollTrigger: {
+                trigger: `#${name}-experience-spacer`,
+                start: "5% center",
+                end: "10% center",
+                scrub: true,
+            },
+        });
+    }, []);
+
     return (
         <div
+            id={`${name}-experience-spacer`}
             className="
                 w-full
                 h-[500vh]
@@ -8,29 +44,101 @@ const Experience = () => {
             "
         >
             <div
+                id={`${name}-experience-container`}
                 className="
                     sticky
                     top-0
                     screen-fit-size
                     flex
+                    flex-col
                     items-center
                     justify-center
+                    -translate-x-[100px]
+                    opacity-0
                 "
             >
                 <div
                     className="
                         flex
-                        sm:flex-row
                         flex-col
                         w-10/12
-                        h-5/6
                         border-gray-400
-                        border-[1px]
                         rounded-lg
+
+                        bg-white/30
+                        backdrop-blur-md
+                        border
+                        border-white/30
                     "
                 >
-                    <div className="flex-1">Hello</div>
-                    <div className="flex-1">World</div>
+                    <div
+                        className="
+                            flex
+                            max-sm:flex-col
+                            flex-row
+                            py-3
+                            sm:px-2
+                            px-4
+                        "
+                    >
+                        <div
+                            className="
+                                flex-1
+                                flex
+                                items-center
+                                justify-center
+                            "
+                        >
+                            <img
+                                id={`${name}-experience-logo`}
+                                alt="storm logo"
+                                src={logoUrl}
+                                className="
+                                    w-[75%]
+                                    opacity-0
+                                "
+                            />
+                        </div>
+                        <div
+                            className="
+                                flex-[2]
+                                flex
+                                flex-col
+                                items-start
+                                justify-center
+                                gap-1
+                            "
+                        >
+                            <h3
+                                className="text-[2rem] text-center"
+                                style={{
+                                    lineHeight: "2.5rem",
+                                }}
+                            >
+                                {corpTitle}
+                            </h3>
+                            <a
+                                href={corpLinkUrl}
+                                title={corpLinkTitle}
+                                className="link"
+                                target="_blank"
+                            >
+                                🔗 {corpLinkTitle}
+                            </a>
+                            <h4>{jobTitle}</h4>
+                            {skillLevel}
+                        </div>
+                    </div>
+                    <div
+                        id={`${name}-experience-description`}
+                        className="
+                            py-3
+                            sm:px-2
+                            px-4
+                        "
+                    >
+                        {description}
+                    </div>
                 </div>
             </div>
         </div>
