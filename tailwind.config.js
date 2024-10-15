e332232dfd6e55d09cb7d6f17faf87b742f8bfe9
SMRSAN <57676740+smrsan@users.noreply.github.com>
2024-10-15 14:46:41 +0330

    Add some new tailwind color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ export default {
     theme: {
         extend: {
             colors: {
+                blue: "#2997FF",
+                gray: {
+                    DEFAULT: "#86868b",
+                    100: "#94928d",
+                    200: "#afafaf",
+                    300: "#42424570",
+                },
+                zinc: "#101010",
                 color: {
                     1: "#AC6AFF",
                     2: "#FFC876",
